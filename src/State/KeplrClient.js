c8124a90a0dfb8ce5f8e6ff1bfda35c623106950
bgok <ken@citizenhodler.com>
2021-05-08 10:42:19 -0700

    Make Keplr Client connection more reliable

--- src/State/KeplrClient.js
+++ src/State/KeplrClient.js
@@ -16,18 +16,19 @@ export const ClientProvider = ({children}) => {
         const waitForKeplr = async () => {
             if (!!window.keplr) {
                 setKeplrLoaded(true)
-                return
+                return window.keplr
             }
 
             if (document.readyState === "complete") {
                 setKeplrLoaded(!!window.keplr)
-                return
+                return window.keplr
             }
 
             return new Promise((resolve) => {
                 const documentStateChange = (event) => {
                     if (event.target?.readyState === "complete") {
                         setKeplrLoaded(!!window.keplr)
+                        resolve(window.keplr)
                         document.removeEventListener("readystatechange", documentStateChange)
                     }
                 };
@@ -40,30 +41,34 @@ export const ClientProvider = ({children}) => {
 
     useEffect(() => {
         const getClient = async () => {
-            // Enabling before using the Keplr is recommended.
-            // This method will ask the user whether or not to allow access if they haven't visited this website.
-            // Also, it will request user to unlock the wallet if the wallet is locked.
-            await window.keplr.enable(CHAIN_ID)
+            if (window?.keplr?.enable) {
+                // Enabling before using the Keplr is recommended.
+                // This method will ask the user whether or not to allow access if they haven't visited this website.
+                // Also, it will request user to unlock the wallet if the wallet is locked.
+                await window.keplr.enable(CHAIN_ID)
 
-            const offlineSigner = window.getOfflineSigner(CHAIN_ID)
-            const enigmaUtils = window.getEnigmaUtils(CHAIN_ID)
+                const offlineSigner = window.getOfflineSigner(CHAIN_ID)
+                const enigmaUtils = window.getEnigmaUtils(CHAIN_ID)
 
-            // You can get the address/public keys by `getAccounts` method.
-            // It can return the array of address/public key.
-            // But, currently, Keplr extension manages only one address/public key pair.
-            // XXX: This line is needed to set the sender address for SigningCosmosClient.
-            const accounts = await offlineSigner.getAccounts()
-            const address = accounts[0].address
-            setAddress(address)
+                // You can get the address/public keys by `getAccounts` method.
+                // It can return the array of address/public key.
+                // But, currently, Keplr extension manages only one address/public key pair.
+                // XXX: This line is needed to set the sender address for SigningCosmosClient.
+                const accounts = await offlineSigner.getAccounts()
+                const address = accounts[0].address
+                setAddress(address)
 
-            // Initialize the gaia api with the offline signer that is injected by Keplr extension.
-            const cosmJS = new SigningCosmWasmClient(
-                API_URL,
-                address,
-                offlineSigner,
-                enigmaUtils
-            )
-            setClient(cosmJS)
+                // Initialize the gaia api with the offline signer that is injected by Keplr extension.
+                const cosmJS = new SigningCosmWasmClient(
+                    API_URL,
+                    address,
+                    offlineSigner,
+                    enigmaUtils
+                )
+                setClient(cosmJS)
+            } else {
+                setKeplrLoaded(false)
+            }
         }
         getClient()
     }, [keplrLoaded])
